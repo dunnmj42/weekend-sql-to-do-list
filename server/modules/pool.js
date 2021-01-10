2df9ef507c158f3e348ef98d6edb4fb2a96476dc
Michael Dunn <dunnmj42@gmail.com>
2021-01-10 11:33:13 -0600

    pool written

---
+++ server/modules/pool.js
@@ -0,0 +1,20 @@
+const pg = require("pg");
+
+const Pool = pg.Pool;
+
+const pool = new Pool({
+  database: "weekend-to-do-app",
+  host: "localhost",
+  port: 5432,
+});
+
+//helper
+pool.on("connect", () => {
+  console.log("PG CONNECTED");
+});
+
+pool.on("error", (error) => {
+  console.log(error);
+});
+
+module.exports = pool;
