e99a6ea220a5159a652f76f8cc2c9100534987a5
Michael Dunn <dunnmj42@gmail.com>
2021-01-10 16:28:59 -0600

    code formatting

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,143 +1,149 @@
-$(document).ready(onReady)
+$(document).ready(onReady);
 
 function onReady() {
-    console.log('JQ READY');
-    setupClickListeners();
-    getTasks();
+  console.log("JQ READY");
+  setupClickListeners();
+  getTasks();
 }
 
 // CLICK LISTENERS AND OBJECT ASSEMBLY
 function setupClickListeners() {
-    $("#taskOutput").on("click", ".markComplete", markComplete);
-    $("#taskOutput").on("click", ".removeBtn", removeButton);
-    $("#addTaskBtn").on("click", function() {
-      console.log("addTaskBtn click");
-  
-      let newTask = {
-        task: $("#taskIn").val(),
-        isImportant: $("#isImportant").val()
-      };
+  $("#taskOutput").on("click", ".markComplete", markComplete);
+  $("#taskOutput").on("click", ".removeBtn", removeButton);
+  $("#addTaskBtn").on("click", function () {
+    console.log("addTaskBtn click");
 
-      if (newTask.task && newTask.isImportant) {
-        addTask(newTask);
-      } else {
-        alert("Please Complete the Form to add a Task");
-      }
-    });
+    let newTask = {
+      task: $("#taskIn").val(),
+      isImportant: $("#isImportant").val(),
+    };
+
+    if (newTask.task && newTask.isImportant) {
+      addTask(newTask);
+    } else {
+      alert("Please Complete the Form to add a Task");
+    }
+  });
 }
 
 // POST
 function addTask(newTask) {
-    console.log("in addTask", newTask);
-    // ajax call to server to POST task
-    $.ajax({
-      type: "POST",
-      url: "/todo",
-      data: newTask,
-    }).then(function (response) {
-        $("#taskIn").val("");
-        $("#isImportant").val("");
-        getTasks();
-    });
+  console.log("in addTask", newTask);
+  // ajax call to server to POST task
+  $.ajax({
+    type: "POST",
+    url: "/todo",
+    data: newTask,
+  }).then(function (response) {
+    $("#taskIn").val("");
+    $("#isImportant").val("");
+    getTasks();
+  });
 }
 
 // GET
 function getTasks() {
-    console.log("in getTasks");
-    // ajax call to server to get tasks
-    $("#taskOutput").empty();
-    $.ajax({
-      type: "GET",
-      url: "/todo",
-    }).then(function (response) {
-      renderTasks(response);
-    });
+  console.log("in getTasks");
+  // ajax call to server to get tasks
+  $("#taskOutput").empty();
+  $.ajax({
+    type: "GET",
+    url: "/todo",
+  }).then(function (response) {
+    renderTasks(response);
+  });
 }
 
 // PUT
 function markComplete() {
-    console.log("clicked markComplete");
-  
-    let task = $(this).closest("tr").data("task");
-    console.log(task);
-  
-    $.ajax({
-      type: "PUT",
-      url: `/todo/${task.id}`,
-      data: task,
+  console.log("clicked markComplete");
+
+  let task = $(this).closest("tr").data("task");
+  console.log(task);
+
+  $.ajax({
+    type: "PUT",
+    url: `/todo/${task.id}`,
+    data: task,
+  })
+    .then(function (response) {
+      console.log("Updated");
+      getTasks();
     })
-      .then(function (response) {
-        console.log("Updated");
-        getTasks();
-      })
-      .catch(function (error) {
-        alert("error updating status");
-      });
+    .catch(function (error) {
+      alert("error updating status");
+    });
 }
 
 // DELETE
 function removeButton() {
-    console.log("clicked remove");
-    swal({
-      title: "Are you sure?",
-      text: "Once removed, you will not be able to recover this Task!",
-      icon: "warning",
-      buttons: true,
-      dangerMode: true,
-    }).then((willDelete) => {
-      if (willDelete) {
-        ajaxDelete();
-        swal("Task Removed!", {
-          icon: "success",
-        });
-      } else {
-        swal("Task not removed!");
-      }
-    });
-  
-    const task = $(this).closest("tr").data("task").id;
-    console.log(task);
-  
-    // delete function being passed to router
-    function ajaxDelete() {
-      $.ajax({
-        type: "DELETE",
-        url: `/todo/${task}`,
-      })
-        .then(function (response) {
-          getTasks();
-        })
-        .catch(function (error) {
-          alert("error in delete");
-        });
+  console.log("clicked remove");
+  swal({
+    title: "Are you sure?",
+    text: "Once removed, you will not be able to recover this Task!",
+    icon: "warning",
+    buttons: true,
+    dangerMode: true,
+  }).then((willDelete) => {
+    if (willDelete) {
+      ajaxDelete();
+      swal("Task Removed!", {
+        icon: "success",
+      });
+    } else {
+      swal("Task not removed!");
     }
+  });
+
+  const task = $(this).closest("tr").data("task").id;
+  console.log(task);
+
+  // delete function being passed to router
+  function ajaxDelete() {
+    $.ajax({
+      type: "DELETE",
+      url: `/todo/${task}`,
+    })
+      .then(function (response) {
+        getTasks();
+      })
+      .catch(function (error) {
+        alert("error in delete");
+      });
+  }
 }
 
 // Render to DOM
 function renderTasks(tasks) {
-    for (let i = 0; i < tasks.length; i++) {
-        let task = tasks[i];
-        let $tr = $("<tr></tr>");
-        let dateAdded = new Date(task.timeAdded).toLocaleDateString('en-us')
+  for (let i = 0; i < tasks.length; i++) {
+    let task = tasks[i];
+    let $tr = $("<tr></tr>");
+    let dateAdded = new Date(task.timeAdded).toLocaleDateString("en-us");
 
-        if(task.isComplete) {
-            $tr = $(`<tr class="table-success"></tr>`);
-        }; // Set row color on complete
+    if (task.isComplete) {
+      $tr = $(`<tr class="table-success"></tr>`);
+    } // Set row color on complete
 
-        $tr.data("task", task);
-        $tr.append(`<td>${task.task}</td>`);
-        $tr.append(`<td>${dateAdded}</td>`);
-        if(task.isImportant) {
-            $tr.append(`<td>Important</td>`);
-        } else {
-            $tr.append(`<td>Not Important</td>`);
-        }
-        if(task.isComplete) {
-            $tr.append(`<td><button class="btn btn-warning markComplete">Mark Not Complete</button></td>`);
-        } else {
-            $tr.append(`<td><button class="btn btn-success markComplete">Mark Complete</button></td>`);
-        }
-        $tr.append(`<td><button class="btn btn-danger removeBtn">Remove</button></td>`);
-        $("#taskOutput").append($tr);
+    $tr.data("task", task);
+    $tr.append(`<td>${task.task}</td>`);
+    $tr.append(`<td>${dateAdded}</td>`);
+    if (task.isImportant) {
+      $tr.append(`<td>Important</td>`);
+    } else {
+      $tr.append(`<td>Not Important</td>`);
     }
+    if (task.isComplete) {
+      $tr.append(
+        `<td><button class="btn btn-warning markComplete">Mark Not Complete</button></td>`
+      );
+    } else {
+      $tr.append(
+        `<td><button class="btn btn-success markComplete">Mark Complete</button></td>`
+      );
+    }
+    $tr.append(
+      `<td><button class="btn btn-danger removeBtn">Remove</button></td>`
+    );
+    $("#taskOutput").append($tr);
+  }
 }
--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -4,80 +4,79 @@ const pool = require("../modules/pool.js"); // DATABASE POOL
 
 // GET
 todoRouter.get("/", (req, res) => {
-    const queryText = `SELECT * FROM "todo" ORDER BY "timeAdded" DESC;`;
-    pool
-      .query(queryText)
-      .then((result) => {
-        console.log(result);
-        res.send(result.rows);
-      })
-      .catch((error) => {
-        console.log(error);
-        res.sendStatus(500);
-      });
+  const queryText = `SELECT * FROM "todo" ORDER BY "timeAdded" DESC;`;
+  pool
+    .query(queryText)
+    .then((result) => {
+      console.log(result);
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.sendStatus(500);
+    });
 });
 
-// POST  
+// POST
 todoRouter.post("/", (req, res) => {
-    let newTask = req.body;
-    console.log(`Adding task`, newTask);
-  
-    let queryText = `INSERT INTO "todo" ("task", "timeAdded", "isImportant")
+  let newTask = req.body;
+  console.log(`Adding task`, newTask);
+
+  let queryText = `INSERT INTO "todo" ("task", "timeAdded", "isImportant")
                        VALUES ($1, current_timestamp, $2);`;
-    pool.query(queryText, [
-        req.body.task,
-        req.body.isImportant
-      ])
-      .then((result) => {
-        res.sendStatus(201);
-      })
-      .catch((error) => {
-        console.log(`Error adding new task`, error);
-        res.sendStatus(500);
-      });
+  pool
+    .query(queryText, [req.body.task, req.body.isImportant])
+    .then((result) => {
+      res.sendStatus(201);
+    })
+    .catch((error) => {
+      console.log(`Error adding new task`, error);
+      res.sendStatus(500);
+    });
 });
 
 // PUT
 todoRouter.put("/:id", (req, res) => {
-    let isComplete = req.body.isComplete;
-    let id = req.params.id;
-    let queryText;
-  
-    console.log(isComplete);
-  
-    if (isComplete === "false") {
-      queryText = `UPDATE "todo"
+  let isComplete = req.body.isComplete;
+  let id = req.params.id;
+  let queryText;
+
+  console.log(isComplete);
+
+  if (isComplete === "false") {
+    queryText = `UPDATE "todo"
                   SET "isComplete" = true
                   WHERE "id" = $1;`;
-    } else if (isComplete === "true") {
-      queryText = `UPDATE "todo"
+  } else if (isComplete === "true") {
+    queryText = `UPDATE "todo"
                   SET "isComplete" = false
                   WHERE "id" = $1;`;
-    }
-  
-    console.log(`Updating task with ${id}, setting isComplete to:`, isComplete);
-  
-    pool.query(queryText, [id])
-      .then((result) => {
-        res.sendStatus(200);
-      })
-      .catch((error) => {
-        console.log(error);
-        res.sendStatus(200);
-      });
+  }
+
+  console.log(`Updating task with ${id}, setting isComplete to:`, isComplete);
+
+  pool
+    .query(queryText, [id])
+    .then((result) => {
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.sendStatus(200);
+    });
 });
 
 // DELETE
 todoRouter.delete("/:id", (req, res) => {
-    let task = req.params.id;
-    console.log("Delete route called for task ", task);
-  
-    const queryText = `DELETE FROM "todo"
+  let task = req.params.id;
+  console.log("Delete route called for task ", task);
+
+  const queryText = `DELETE FROM "todo"
                         WHERE "id" = $1;`;
-  
-    pool.query(queryText, [task]).then((results) => {
-      res.sendStatus(204);
-    });
+
+  pool.query(queryText, [task]).then((results) => {
+    res.sendStatus(204);
+  });
 });
 
-  module.exports = todoRouter;
+module.exports = todoRouter;
