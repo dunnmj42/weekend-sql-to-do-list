7ccd76767f779da87873a5aeddeae883692a3010
Michael Dunn <dunnmj42@gmail.com>
2021-01-10 12:05:56 -0600

    GET route established and tested

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,6 +2,42 @@ $(document).ready(onReady)
 
 function onReady() {
     console.log('JQ READY');
-    setupClickListeners();
+    //setupClickListeners();
     getTasks();
+}
+
+// GET
+function getTasks() {
+    console.log("in getTasks");
+    // ajax call to server to get tasks
+    $("#taskOutput").empty();
+    $.ajax({
+      type: "GET",
+      url: "/todo",
+    }).then(function (response) {
+      renderTasks(response);
+    });
+}
+
+function renderTasks(tasks) {
+    for (let i = 0; i < tasks.length; i++) {
+        let task = tasks[i];
+        let $tr = $("<tr></tr>");
+
+        $tr.data("task", task);
+        $tr.append(`<td>${task.task}</td>`);
+        $tr.append(`<td>${task.timeAdded}</td>`);
+        if(task.isImportant) {
+            $tr.append(`<td>Important</td>`);
+        } else {
+            $tr.append(`<td>Not Important</td>`);
+        }
+        if(task.isComplete) {
+            $tr.append(`<td>Complete</td>`);
+        } else {
+            $tr.append(`<td>Not Complete</td>`);
+        }
+        $tr.append(`<td><button class="removeBtn">Remove</button></td>`);
+        $("#taskOutput").append($tr);
+    }
 }
--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -2,3 +2,19 @@ const express = require("express");
 const todoRouter = express.Router();
 const pool = require("../modules/pool.js"); // DATABASE POOL
 
+// GET
+todoRouter.get("/", (req, res) => {
+    const queryText = `SELECT * FROM "todo" ORDER BY "timeAdded" DESC;`;
+    pool
+      .query(queryText)
+      .then((result) => {
+        console.log(result);
+        res.send(result.rows);
+      })
+      .catch((error) => {
+        console.log(error);
+        res.sendStatus(500);
+      });
+  });
+
+  module.exports = todoRouter;
